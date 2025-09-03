4977da473760c0d2387b9e50c701b6cfa7ddabb1
linuxdaemon <linuxdaemon.irc@gmail.com>
2025-09-03 21:29:30 +0000

    ci: switch release workflow to trigger from tag
    This will ensure the workflow is triggered at the right time

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 changelog_file="$(mktemp)"
-cz bump --changelog-to-stdout > "$changelog_file"
+cz bump --changelog-to-stdout --git-output-to-stderr > "$changelog_file"
 
 git push origin main --tags
 
